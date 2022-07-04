07d3e97d65617c3bb67b22dec1d8847b29489500
Vatsal Raina <vatsalraina@hotmail.co.uk>
2022-07-04 15:55:31 +0100

    evaluate on validation during training and save best model

--- reclor/baseline/train.py
+++ reclor/baseline/train.py
@@ -29,7 +29,9 @@ parser.add_argument('--dropout', type=float, default=0.1, help='Specify the drop
 parser.add_argument('--n_epochs', type=int, default=10, help='Specify the number of epochs to train for')
 parser.add_argument('--seed', type=int, default=1, help='Specify the global random seed')
 parser.add_argument('--train_data_path', type=str, help='Load path of training data')
-parser.add_argument('--save_path', type=str, help='Load path to which trained model will be saved')
+parser.add_argument('--val_data_path', type=str, help='Load path of validation data')
+parser.add_argument('--val_interval', type=int, default=1, help='Epoch intervals for evaluating on validation set')
+parser.add_argument('--save_path', type=str, help='Load path to which best trained model will be saved')
 parser.add_argument('--ranking_type', type=str, default='random', help='Ranking order for training data')
 parser.add_argument('--data_frac', type=float, default=0.1, help='Specify the fraction of data to use')
 
@@ -38,10 +40,7 @@ def format_time(elapsed):
     '''
     Takes a time in seconds and returns a string hh:mm:ss
     '''
-    # Round to the nearest second.
     elapsed_rounded = int(round((elapsed)))
-
-    # Format as hh:mm:ss
     return str(datetime.timedelta(seconds=elapsed_rounded))
 
 # Set device
@@ -79,17 +78,16 @@ def main(args):
     # Choose device
     device = get_default_device()
 
-    with open(args.train_data_path) as f:
-        train_data = json.load(f)
-
     electra_base = "google/electra-base-discriminator"
     electra_large = "google/electra-large-discriminator"
     tokenizer = ElectraTokenizer.from_pretrained(electra_large, do_lower_case=True)
 
+    with open(args.train_data_path) as f:
+        train_data = json.load(f)
+
     labels = []
     input_ids = []
     token_type_ids = []
-    count = 0
 
     for item in train_data:
         context = item["context"]
@@ -117,6 +115,38 @@ def main(args):
             sen_attention_masks.append(att_mask)
         attention_masks.append(sen_attention_masks)
 
+    with open(args.val_data_path) as f:
+        val_data = json.load(f)
+
+    labels_val = []
+    input_ids_val = []
+    token_type_ids_val = []
+
+    for item in val_data:
+        context = item["context"]
+        question = item["question"]
+        lab = item["label"]
+        labels_val.append(lab)
+        four_inp_ids = []
+        four_tok_type_ids = []
+        for i, ans in enumerate(item["answers"]):
+            combo = context + " [SEP] " + question + " " + ans
+            inp_ids = tokenizer.encode(combo)
+            tok_type_ids = [0 if i<= inp_ids.index(102) else 1 for i in range(len(inp_ids))]
+            four_inp_ids.append(inp_ids)
+            four_tok_type_ids.append(tok_type_ids)
+        four_inp_ids = pad_sequences(four_inp_ids, maxlen=MAXLEN, dtype="long", value=0, truncating="post", padding="post")
+        four_tok_type_ids = pad_sequences(four_tok_type_ids, maxlen=MAXLEN, dtype="long", value=0, truncating="post", padding="post")
+        input_ids_val.append(four_inp_ids)
+        token_type_ids_val.append(four_tok_type_ids)
+    # Create attention masks
+    attention_masks_val = []
+    for sen in input_ids_val:
+        sen_attention_masks = []
+        for opt in sen:
+            att_mask = [int(token_id > 0) for token_id in opt]
+            sen_attention_masks.append(att_mask)
+        attention_masks_val.append(sen_attention_masks)
 
 
     # Apply some ranking operation
@@ -141,11 +171,21 @@ def main(args):
     attention_masks = torch.tensor(attention_masks)
     attention_masks = attention_masks.long().to(device)
 
-    # Create the DataLoader for training set.
+    labels_val = torch.tensor(labels_val)
+    labels_val = labels_val.long().to(device)
+    input_ids_val = torch.tensor(input_ids_val)
+    input_ids_val = input_ids_val.long().to(device)
+    token_type_ids_val = torch.tensor(token_type_ids_val)
+    token_type_ids_val = token_type_ids_val.long().to(device)
+    attention_masks_val = torch.tensor(attention_masks_val)
+    attention_masks_val = attention_masks_val.long().to(device)
+
     train_data = TensorDataset(input_ids, token_type_ids, attention_masks, labels)
     train_sampler = RandomSampler(train_data)
     train_dataloader = DataLoader(train_data, sampler=train_sampler, batch_size=args.batch_size)
 
+    val_data = TensorDataset(input_ids_val, token_type_ids_val, attention_masks_val, labels_val)
+    val_dataloader = DataLoader(val_data, batch_size=args.batch_size, shuffle=False)
 
     model = ElectraForMultipleChoice.from_pretrained(electra_large).to(device)
 
@@ -157,32 +197,26 @@ def main(args):
 
     loss_values = []
 
-    # Total number of training steps is number of batches * number of epochs.
     total_steps = len(train_dataloader) * args.n_epochs
-    # Create the learning rate scheduler.
     scheduler = get_linear_schedule_with_warmup(optimizer,
                                                 num_warmup_steps = 0.1*total_steps,
                                                 num_training_steps = total_steps)
 
 
+    best_val_loss = 100
+    file_path = args.save_path+'electra_QA_MC_seed'+str(args.seed)+'.pt'
+
     for epoch in range(args.n_epochs):
-        # Perform one full pass over the training set.
         print("")
         print('======== Epoch {:} / {:} ========'.format(epoch + 1, args.n_epochs))
         print('Training...')
-        # Measure how long the training epoch takes.
         t0 = time.time()
-        # Reset the total loss for this epoch.
         total_loss = 0
         model.train()
         model.zero_grad()
-        # For each batch of training data...
         for step, batch in enumerate(train_dataloader):
-            # Progress update every 40 batches.
             if step % 40 == 0 and not step == 0:
-                # Calculate elapsed time in minutes.
                 elapsed = format_time(time.time() - t0)
-                # Report progress.
                 print('  Batch {:>5,}  of  {:>5,}.    Elapsed: {:}.'.format(step, len(train_dataloader), elapsed))
             b_input_ids = batch[0].to(device)
             b_tok_typ_ids = batch[1].to(device)
@@ -192,33 +226,34 @@ def main(args):
             outputs = model(input_ids=b_input_ids, attention_mask=b_att_msks, token_type_ids=b_tok_typ_ids, labels=b_labs)
             loss = outputs[0]
             total_loss += loss.item()
-            print(loss.item())
+            # print(loss.item())
             optimizer.zero_grad()
             loss.backward()
-            # Clip the norm of the gradients to 1.0.
-            # This is to help prevent the "exploding gradients" problem.
-            # torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
-            # if (step+1) % accumulation_steps == 0:
-            # Update parameters and take a step using the computed gradient.
-            # The optimizer dictates the "update rule"--how the parameters are
-            # modified based on their gradients, the learning rate, etc.
             optimizer.step()
-            # Update the learning rate.
             scheduler.step()
-                # model.zero_grad()
-        # Calculate the average loss over the training data.
         avg_train_loss = total_loss / len(train_dataloader)
-
-        # Store the loss value for plotting the learning curve.
         loss_values.append(avg_train_loss)
 
         print("")
         print("  Average training loss: {0:.2f}".format(avg_train_loss))
         print("  Training epoch took: {:}".format(format_time(time.time() - t0)))
 
-    # Save the model to a file
-    file_path = args.save_path+'electra_QA_MC_seed'+str(args.seed)+'.pt'
-    torch.save(model, file_path)
+        if (epoch + 1) % args.val_interval == 0:
+            model.eval()
+            total_loss_val = 0
+            with torch.no_grad():
+                for batch in val_dataloader:
+                    b_input_ids = batch[0].to(device)
+                    b_tok_typ_ids = batch[1].to(device)
+                    b_att_msks = batch[2].to(device)
+                    b_labs = batch[3].to(device)
+                    outputs = model(input_ids=b_input_ids, attention_mask=b_att_msks, token_type_ids=b_tok_typ_ids, labels=b_labs)
+                    loss = outputs[0]
+                    total_loss += loss.item()
+                avg_val_loss = total_loss_val / len(val_dataloader)
+            print("  Average validation loss: {0:.2f}".format(avg_val_loss))
+            if avg_val_loss < best_val_loss:
+                torch.save(model, file_path)
 
 if __name__ == '__main__':
     args = parser.parse_args()
